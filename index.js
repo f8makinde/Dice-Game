65b7f5ed1cee67208bd9c02fa5641a981b92c228
Faith Makinde <faithmakinde0@gmail.com>
2022-09-12 13:02:22 +0100

    setting up the DOM nodes

---
+++ index.js
@@ -0,0 +1,7 @@
+const player1Dice = document.getElementById("player1Dice");
+const player2Dice = document.getElementById("player2Dice");
+const player1Scoreboard = document.getElementById("player1Scoreboard");
+const player2Scoreboard = document.getElementById("player2Scoreboard");
+const messageEl = document.getElementById("message");
+const rollBtn = document.getElementById("rollBtn");
+const resetBtn = document.getElementById("resetBtn");
